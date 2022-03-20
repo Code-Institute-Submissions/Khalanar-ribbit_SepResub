6869c942b24ff317acdab688d187c2e27e80cea3
Khalanar <pablo.xez@gmail.com>
2022-03-20 12:32:49 +0000

    add Procfile

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["ribbit-0.herokuapp.com", "localhost"]
 
 
 # Application definition
