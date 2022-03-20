fff7839ac785adf5b08db06faa892045b6605111
Khalanar <pablo.xez@gmail.com>
2022-03-20 11:44:14 +0000

    prepare db settings for deployment onto Heroku

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os.path
+import os
+import dj_database_url
+
+if os.path.isfile('env.py'):
+    import env
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x(satv)7o^%=_fn6=ap9ul7r8%b^qg&mfg3vx3)k-=xmmp=po!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,13 +82,16 @@ WSGI_APPLICATION = 'ribbit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- wall/models.py
+++ wall/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+'''
+class Post(models.Model):
+    title
+    content
+    excerpt
+    category
+    author
+    date_created
+    date_modified
+
+
+class Comment(models.Model):
+    author
+    post
+    date_created
+
+class Category(models.Model):
+    title
+    handle
+'''
