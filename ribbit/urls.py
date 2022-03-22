d2f91b67d6c76f823e714601e995ad1ff0a2c8c0
Khalanar <pablo.xez@gmail.com>
2022-03-22 13:51:10 +0000

    set up user authentication

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('wall.urls'), name="blog_urls")
+    path('', include('wall.urls'), name="blog_urls"),
+    path('accounts/', include('allauth.urls'))
 ]
