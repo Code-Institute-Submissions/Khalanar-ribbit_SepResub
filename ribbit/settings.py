d869ad59bb51c8adb7bb9df4ae8c652dac4a79f3
Pablo Sanchez <pablo.xez@gmail.com>
2022-04-12 15:28:06 +0100

    Update settings.py
    Fix database connection

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -98,15 +98,8 @@ WSGI_APPLICATION = 'ribbit.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
